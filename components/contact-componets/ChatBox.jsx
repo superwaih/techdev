f4652b37d68b61dc442819a6557b0d41bb06a1bd
shittu adewale <sheriffhardewale@gmail.com>
2022-11-28 23:25:26 +0100

    fixed the design

--- components/contact-componets/ChatBox.jsx
+++ components/contact-componets/ChatBox.jsx
@@ -4,7 +4,7 @@ import image1 from "../imgs/swipeimgs/image1.png"
 const ChatBox = () => {
   return (
     <div className='m-auto flex flex-col md:flex-row pb-20 md:pb-32  w-[85%]'>
-        <div className='bg-black flex-1 text-white py-12 rounded-md
+        <div className='bg-black flex-1  text-white py-12 rounded-md
         px-12 space-y-8'>
             <div className='font-bold text-xl md:text-3xl' >
                 <h3>Got a project?</h3>
@@ -12,7 +12,7 @@ const ChatBox = () => {
             </div>
             <p  className='leading-[45px] text-xl ,md:text-2xl font-bold max-w-md'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
         </div>
-        <div className='flex-1 md:relative md:mt-4 md:mb-4 bg-orange'>
+        <div className='flex-1  2xl:max-w-[500px] md:relative md:mt-4 md:mb-4 bg-orange'>
             
                 <Image
                     width={400}
@@ -20,7 +20,7 @@ const ChatBox = () => {
                     objectFit="cover"
                     src={image1}
                     alt=""
-                    className='md:absolute top-8 rounded-md -left-[10%]'
+                    className='md:absolute top-8 rounded-md -left-[2%]'
                 />
             
 
